0d1d7d5f8bfd782b5cc3e52865eab10fd01abf12
Chun-Bae <dbtjrdla2056@gmail.com>
2025-04-23 20:09:44 +0900

    nav bar

--- lib/presentation/screens/parent/parent_screen.dart
+++ lib/presentation/screens/parent/parent_screen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class ParentScreen extends StatelessWidget {
+class ParentScreen extends StatefulWidget {
   const ParentScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ParentScreen> createState() => _ParentScreenState();
+}
+
+class _ParentScreenState extends State<ParentScreen> {
+  int _selectedIndex = 0;
+
+  final List<String> _routes = [
+    '/parent/home',
+    '/parent/messages',
+    '/parent/notifications',
+    '/parent/settings',
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    context.go(_routes[index]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +36,29 @@ class ParentScreen extends StatelessWidget {
           '부모 홈 화면입니다.',
           style: TextStyle(fontSize: 20),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: '메시지',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: '알림',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '설정',
+          ),
+        ],
       ),
     );
   }
